4c1b369fac9033a6af668fb42d8ad31958360bc6
yohanesrioirsan <rioirsan8@gmail.com>
2025-11-05 06:02:00 +0700

    first init

---
+++ containers/Hero/Hero.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+interface HeroProps {
+  setStepper: () => void;
+}
+
+export default function Hero({ setStepper }: HeroProps) {
+  return (
+    <div className="flex flex-col items-center gap-3 w-full">
+      <h1 className="text-[44px] font-bold text-center text-white lg:w-1/2 drop-shadow">
+        Generate The Prompt Based On Your Imaginary
+      </h1>
+      <div className="flex items-center gap-3">
+        <Button variant="primary" onClick={setStepper}>
+          Generate Prompt Now
+        </Button>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button variant="secondary">How this work?</Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>
+              Don’t worry about sounding fancy — just type whatever’s in your
+              head, and we’ll shape it into a great prompt for you.
+            </p>
+          </TooltipContent>
+        </Tooltip>
+      </div>
+    </div>
+  );
+}

e9470f9c6ef8d0ad764abf496929f0c8e6379f93
yohanesrioirsan <rioirsan8@gmail.com>
2025-11-11 09:28:24 +0700

    update: set rate limiter for POST request

--- app/api/send-request/route.ts
+++ app/api/send-request/route.ts
@@ -1,6 +1,8 @@
 import { createClient } from "@/utils/supabase/server";
 import { NextRequest, NextResponse } from "next/server";
 
+const requestLog = new Map<string, number>();
+
 type RequestBody = {
   plain_prompt: string;
   generated_prompt: string;
@@ -8,8 +10,50 @@ type RequestBody = {
   ai_model: string;
 };
 
+function rateLimiter(id: string): { allowed: boolean; resetTime?: number } {
+  if (process.env.NODE_ENV === "development") {
+    return { allowed: true };
+  }
+
+  const now = Date.now();
+  const lastRequest = requestLog.get(id);
+  const requestTime = 15 * 60 * 1000;
+
+  if (lastRequest && now - lastRequest < requestTime) {
+    const resetTime = lastRequest + requestTime;
+    return { allowed: false, resetTime };
+  }
+
+  requestLog.set(id, now);
+
+  for (const [key, timestamp] of requestLog.entries()) {
+    if (now - timestamp > requestTime) {
+      requestLog.delete(key);
+    }
+  }
+
+  return { allowed: true };
+}
+
 export async function POST(request: NextRequest) {
   try {
+    const userAddress =
+      request.headers.get("x-forwarded-for")?.split(",")[0] ??
+      request.headers.get("x-real-ip") ??
+      "127.0.0.1";
+
+    const { allowed, resetTime } = rateLimiter(userAddress);
+
+    if (!allowed && resetTime) {
+      const waitTime = Math.ceil((resetTime - Date.now()) / 1000 / 60);
+
+      return NextResponse.json({
+        message: `Rate Limit exceeded. Please Try Again in ${waitTime} minutes`,
+        success: false,
+        status: 429,
+      });
+    }
+
     const supabase = await createClient();
     const body: RequestBody = await request.json();
 
